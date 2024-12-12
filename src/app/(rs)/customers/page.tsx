f7d1947483cc622735641ab15781c37c199ad48c
Yang <y953159141@gmail.com>
2024-12-12 00:59:54 -0800

    react table

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,5 +1,7 @@
 import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
+import CustomerTable from "@/app/(rs)/customers/CustomerTable";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+import * as Sentry from "@sentry/nextjs";
 
 export const metadata = {
   title: "Customer Search",
@@ -11,13 +13,20 @@ export default async function Customers({
 }) {
   const { searchText } = await searchParams;
   if (!searchText) return <CustomerSearch />;
+  const span = Sentry.startInactiveSpan({
+    name: "getCustomerSearchResults-2",
+  });
   const results = await getCustomerSearchResults(searchText);
-  console.log({ results });
+  span.end();
   return (
     <>
       <CustomerSearch />
 
-      <p>{JSON.stringify(results)}</p>
+      {results.length ? (
+        <CustomerTable data={results} />
+      ) : (
+        <p className="mt-4">No results found</p>
+      )}
     </>
   );
 }
